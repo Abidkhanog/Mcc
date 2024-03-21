2aa9cd3f2aa2fe1305d9799fdaa01cd852b411d0
Abidkhanog <145998853+Abidkhanog@users.noreply.github.com>
2024-03-21 11:02:59 +0530

    Add files via upload

---
+++ 7)GUI/MyApplication6/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText number1, number2;
+    Button addButton, subtractButton, multiplyButton, divideButton;
+    TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        number1 = findViewById(R.id.number1);
+        number2 = findViewById(R.id.number2);
+        addButton = findViewById(R.id.addButton);
+        subtractButton = findViewById(R.id.subtractButton);
+        multiplyButton = findViewById(R.id.multiplyButton);
+        divideButton = findViewById(R.id.divideButton);
+        result = findViewById(R.id.result);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double num1 = Double.parseDouble(number1.getText().toString());
+                double num2 = Double.parseDouble(number2.getText().toString());
+                double res = num1 + num2;
+                result.setText("Result: " + res);
+            }
+        });
+
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double num1 = Double.parseDouble(number1.getText().toString());
+                double num2 = Double.parseDouble(number2.getText().toString());
+                double res = num1 - num2;
+                result.setText("Result: " + res);
+            }
+        });
+
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double num1 = Double.parseDouble(number1.getText().toString());
+                double num2 = Double.parseDouble(number2.getText().toString());
+                double res = num1 * num2;
+                result.setText("Result: " + res);
+            }
+        });
+
+        divideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double num1 = Double.parseDouble(number1.getText().toString());
+                double num2 = Double.parseDouble(number2.getText().toString());
+                double res = num1 / num2;
+                result.setText("Result: " + res);
+            }
+        });
+    }
+}
